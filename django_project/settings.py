bfedb0071124090314bbbfb44c7b95fecece9f5a
Brent Jure <brentjure@gmail.com>
2023-07-21 12:53:42 +0300

    added schema and documentation

--- django_project/settings.py
+++ django_project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount",
     "dj_rest_auth",
     "dj_rest_auth.registration",
+    "drf_spectacular",
     # local
     "accounts.apps.AccountsConfig",
     "posts.apps.PostsConfig",
@@ -60,8 +61,14 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Blog API project",
+    "DESCRIPTION": "A simple blog to learn about DRF",
+    "VERSION": "1.0.0",
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
